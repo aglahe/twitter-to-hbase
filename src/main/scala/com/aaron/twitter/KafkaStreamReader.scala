6f08266e6ce76baa0bdfb18f166cbfad0b09c821
Aaron Glahe <aaron.glahe@gmail.com>
2016-05-25 17:55:16 -0400

    added logger

--- src/main/scala/com/aaron/twitter/KafkaStreamReader.scala
+++ src/main/scala/com/aaron/twitter/KafkaStreamReader.scala
@@ -12,11 +12,11 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
-import org.apache.log4j.{Level, Logger}
+import org.apache.log4j.Logger
 
 import it.nerdammer.spark.hbase._
 
-object KafkaStreamReader {
+object KafkaStreamReader extends LogHelper {
 
     def main(args: Array[String]) {
 
@@ -25,16 +25,16 @@ object KafkaStreamReader {
             System.exit(1)
         }
 
-        println("Setting up conf...")
+        logger.debug("Setting up conf...")
         val conf = new SparkConf().setAppName("Twitter to HBase")
         val ssc = new StreamingContext(conf, Milliseconds(500))
 
-        println("Set up Kafka Parms..")
+        logger.debug("Set up Kafka Parms..")
         val Array(brokers, groupid, topic, hbaseTable) = args
         val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers, "group.id" -> groupid)
         val topics = Set[String](topic)
 
-        println("Setting up stream...")
+        logger.debug("Setting up stream...")
         val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
 
         // Get the value, and convert the RDD[String] into a DataFrame using JSON
@@ -45,18 +45,20 @@ object KafkaStreamReader {
 
                 // Create KV RDD with _1 being the rowkey
                 // We need to put the formats = DefaultFormats in here because of a bug in json4s 3.2.10/11
-                // Also we needed to define MessageDigest here
                 val kvRdd = jsonRdd.map(v => {implicit val formats = DefaultFormats;
                     (DatatypeConverter.printHexBinary(md5((v._1\"user"\"id").extract[String]))+"."+(v._1\"id").extract[String], v._2)})
 
                 // Write to HBase
-                println("writing 1st rowkey: "+ kvRdd.first()._1)
+                logger.debug("writing 1st rowkey: "+ kvRdd.first()._1)
                 kvRdd.toHBaseTable(hbaseTable).toColumns("data").inColumnFamily("d").save()
+            }
+            else {
+                logger.debug("Rdd was empty")
             }
         })
 
         // Start the fun
-        println("Start processing...")
+        logger.debug("Start processing...")
         ssc.start()
         ssc.awaitTermination()
     }
@@ -64,4 +66,9 @@ object KafkaStreamReader {
     def md5(s: String) = {
         MessageDigest.getInstance("MD5").digest(s.getBytes)
     }
+}
+
+trait LogHelper {
+    lazy val logger = Logger.getLogger(getClass.getName)
+
 }
